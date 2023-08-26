7870f5a2e18d628250a499f187d6fc20396ef3af
Azat Khamzin <AzRKhamzin@gmail.com>
2023-08-27 02:30:12 +0300

    add database initial migration

---
+++ internal/pg/migrations/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL,
+    CONSTRAINT "pk_user_id" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS segments (
+    id SERIAL NOT NULL,
+    slug VARCHAR UNIQUE NOT NULL,
+    CONSTRAINT "pk_user_id" PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_segment (
+    user_id INTEGER REFERENCES users (id),
+    seg_id INTEGER REFERENCES segments (id)
+);
