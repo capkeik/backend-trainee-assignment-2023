bfff90459e6c69d3a32f296a889eb8215d7045b6
Azat Khamzin <AzRKhamzin@gmail.com>
2023-08-27 02:24:25 +0300

    add config loading

--- cmd/segmentation/main.go
+++ cmd/segmentation/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"github.com/capkeik/backend-trainee-assignment-2023/internal/config"
+	"log"
+)
 
 func main() {
-	fmt.Println("Starting Segmentation Service")
+	log.Println("Starting Segmentation Service")
+	cfg := config.Get()
+	_ = cfg
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/capkeik/backend-trainee-assignment-2023
 
 go 1.21.0
+
+require github.com/kelseyhightower/envconfig v1.4.0 // indirect
